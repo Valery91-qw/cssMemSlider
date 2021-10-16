9fce710364068587a5ec405292a33cdaef766cf1
Valery91-qw <vakriv91@gmail.com>
2021-10-16 14:26:21 +0300

    feat: add changing description (Saturday, October 16, 2021 14:26:00)

--- src/index.js
+++ src/index.js
@@ -3,17 +3,24 @@ const controls = document.querySelectorAll('li')
 const description = document.querySelector('.mem-description')
 
 const images  = ['./assets/mem1.jpg', './assets/mem2.jpg', './assets/mem3.jpg', './assets/mem4.jpg']
-
+const descriptionText = [
+    'Когда же завезут ??',
+    'Надо кормить !!!',
+    'Куриное филе ?',
+    'Do you know what I meme ?'
+]
 
 controls.forEach(control => {
     control.addEventListener('click', (event) => {
-        changeImage(event.currentTarget.dataset.index)
+        let position = event.currentTarget.dataset.index
+        changeImage(position)
+        changeDescription(position)
     })
 })
 
-function changeImage(data) {
-    imageBox.style.backgroundImage = `url(${images[data]})`
+function changeImage(position) {
+    imageBox.style.backgroundImage = `url(${images[position]})`
 }
-function changeDescription(data) {
-
+function changeDescription(position) {
+    description.textContent = descriptionText[position]
 }
