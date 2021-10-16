b1256eb0700481fb67fd2883bb0ff851a8b98ac2
Valery91-qw <vakriv91@gmail.com>
2021-10-16 14:19:45 +0300

    feat: add changing images (Saturday, October 16, 2021 14:19:10)

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 const imageBox = document.querySelector('.slider')
 const controls = document.querySelectorAll('li')
+const description = document.querySelector('.mem-description')
+
+const images  = ['./assets/mem1.jpg', './assets/mem2.jpg', './assets/mem3.jpg', './assets/mem4.jpg']
+
 
 controls.forEach(control => {
     control.addEventListener('click', (event) => {
-        console.log(event.currentTarget.dataset.index)
+        changeImage(event.currentTarget.dataset.index)
     })
 })
+
+function changeImage(data) {
+    imageBox.style.backgroundImage = `url(${images[data]})`
+}
+function changeDescription(data) {
+
+}
